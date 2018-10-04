382041ab2e78177e76dc361d7b10792f9050ebf0
XIE Yijie <D.B.@XIEs-MacBook-Air.local>
2018-10-04 20:11:45 +0800

    Generated api and defined attributes for Event object

---
+++ api/models/Event.js
@@ -0,0 +1,39 @@
+/**
+ * Event.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+    name: { type: "string" },
+    shortInfo: { type: "string" },
+    fullInfo: { type: "string" },
+    imageSrc: { type: "string" },
+    organizer: { type: "string" },
+    start: { type: "string", columnType: "date" },
+    end: { type: "string", columnType: "date" },
+    venue: { type: "string" },
+    quota: { type: "nuumber" },
+    highlighted: { type: "boolean" }
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+
