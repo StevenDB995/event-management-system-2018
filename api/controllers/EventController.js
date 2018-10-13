1818c92e57782162190e22d94dede8fd71ac57e6
XIE Yijie <D.B.@XIEs-MacBook-Air.local>
2018-10-14 00:05:07 +0800

    Created create page and action

--- api/controllers/EventController.js
+++ api/controllers/EventController.js
@@ -6,7 +6,18 @@
  */
 
 module.exports = {
-  
+    // action - create
+    create: async function(req, res) {
+        
+        if (req.method == "GET")
+            return res.view('event/create');
 
+        if (typeof req.body.Event === "undefined")
+            return res.badRequest("Form-data not received.");
+
+        await Event.create (req.body.Event);
+
+        return res.ok("Successfully created!");
+    }
 };
 
