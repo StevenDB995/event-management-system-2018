7b912fbc295facf40a0c4977262a6069600adbfa
XIE Yijie <D.B.@XIEs-MacBook-Air.local>
2018-11-10 16:26:20 +0800

    Further refinements

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -45,7 +45,7 @@ module.exports.bootstrap = async function (done) {
       fullInfo: 'The International Festival is an event which HKBU holds on campus every October. The Festival presents a national costume parade, variety show, food, talks, exhibitions, workshops and many more... Click the URL to view the details of this programme: https://intl.hkbu.edu.hk/f/upload/1021/Intl_Fest_2018.pdf',
       imageSrc: 'https://intl.hkbu.edu.hk/f/event/1367/Poster_430x597_B03c.jpg',
       organizer: 'Student Affairs',
-      date: new Date('2018/10/23'),
+      date: new Date('2018-10-23'),
       startTime: '09:00', endTime: '21:00',
       venue: 'HKBU Campus',
       box: 'highlighted',
@@ -58,7 +58,7 @@ module.exports.bootstrap = async function (done) {
       fullInfo: '一年一度既HKBU Singing Contest又到喇！身為浸大人既你點可能錯過！？無論你係咩系、Bechelor/AD Programme，我地都歡迎你報名！身邊有唱得之人就更係要推薦佢黎喇！詳情可閱Poster或去到報名Counter查詢。',
       imageSrc: 'https://images.cdn3.stockunlimited.net/preview1300/singing-contest-design_1992565.jpg',
       organizer: 'Student Union',
-      date: new Date('2018/11/03'),
+      date: new Date('2018-11-03'),
       startTime: '19:00', endTime: '21:30',
       venue: 'AC Hall',
       quota: 500,
@@ -72,7 +72,7 @@ module.exports.bootstrap = async function (done) {
       fullInfo: 'Learn English and have some fun! Check the poster for more information.',
       imageSrc: 'http://via.placeholder.com/430x597/dec.png',
       organizer: 'Language Centre',
-      date: new Date('2018/11/15'),
+      date: new Date('2018-11-15'),
       startTime: '18:30', endTime: '21:30',
       venue: 'RRS401',
       quota: 90,
@@ -84,7 +84,7 @@ module.exports.bootstrap = async function (done) {
       name: 'Real Craftmanship',
       shortInfo: "Have an idea of real craftmanship? Join us and you'll learn some!",
       imageSrc: 'http://via.placeholder.com/430x597/dec.png',
-      date: new Date('2018/11/17'),
+      date: new Date('2018-11-17'),
       organizer: "Academy of Visual Arts",
       startTime: '13:30', endTime: '17:30',
       venue: 'CVA112',
@@ -98,7 +98,7 @@ module.exports.bootstrap = async function (done) {
       shortInfo: 'Come and be the best 3 points shooter here! Every basketball lovers are welcomed!',
       fullinfo: 'Come and be the best 3 points shooter here! Every basketball lovers are welcomed! Scan the QR code for enrollment.',
       imageSrc: 'http://via.placeholder.com/430x597/dec.png',
-      date: new Date('2018/11/21'),
+      date: new Date('2018-11-21'),
       organizer: "UASE",
       startTime: '14:30', endTime: '16:00',
       venue: 'RRS401',
@@ -113,7 +113,7 @@ module.exports.bootstrap = async function (done) {
       fullInfo: 'kkkij lmnhhg @#$%^&*><?+=...',
       imageSrc: 'http://via.placeholder.com/430x597/dec.png',
       organizer: "Student Affairs",
-      date: new Date('2018/11/23'),
+      date: new Date('2018-11-23'),
       startTime: '15:00', endTime: '18:00',
       venue: 'AAB201',
       quota: 180,
@@ -125,7 +125,8 @@ module.exports.bootstrap = async function (done) {
   await User.createEach([
     { "username": "StevenDB", "password": await sails.bcrypt.hash('123456', saltRounds), role: "admin", id: 1 },
     { "username": "16251296", "password": await sails.bcrypt.hash('222222', saltRounds), role: "student", id: 2 },
-    { "username": "16251210", "password": await sails.bcrypt.hash('444444', saltRounds), role: "student", id: 3 }
+    { "username": "16251210", "password": await sails.bcrypt.hash('444444', saltRounds), role: "student", id: 3 },
+    { "username": "16251458", "password": await sails.bcrypt.hash('666666', saltRounds), role: "student", id: 4 }
   ]);
 
   // ```
